5d42c252cc34c6dc3b9a72b44f51b5acaa0b02d0
pppreap <paulap@Paulas-MacBook-Pro.local>
2022-07-07 15:45:59 -0400

    weather icon added and api for UV display

--- script.js
+++ script.js
@@ -9,50 +9,50 @@ var wind = document.querySelector('.wind');
 var humid = document.querySelector('.humid');
 var uv = document.querySelector('.uv'); 
 
-
-
 /*right apis used
- uv api: https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=hourly&appid=dd4aacd5d922b0989a6c7cc5ba3268d7
-forecast api: api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}   &units=imperial
-
-stating key  
 const key = dd4aacd5d922b0989a6c7cc5ba3268d7;*/
 
-//on event of clicking button of city input , data is retrieved json parsed response, and use DOM to access property of datas
+//on event of clicking button of city input, data is retrieved json parsed response, and use DOM to access property of datas
 button.addEventListener('click',function(){
 fetch('https://api.openweathermap.org/data/2.5/forecast?q='+inputValue.value+'&appid=dd4aacd5d922b0989a6c7cc5ba3268d7&units=imperial')
 .then(response => response.json())
-// .then(data => console.log(data))
 .then(data=> {
+
         var cityName = data["city"]["name"];
         city.innerHTML= cityName;
         //var dateName = data["list"][0]["dt_txt"];
-        //date.innerHTML = dateName;
-
-    //var iconName = data["list"][0]["weather"][0]["icon"];
-    //icon.innerHTML = iconName;
-
+        //display icon with openweather icon src  URL is http://openweathermap.org/img/wn/10d@2x.png
+         var iconName = data["list"][0]["weather"][0]["icon"];
+         let url = "http://openweathermap.org/img/wn/" + iconName + "@2x.png";
+         icon.src = url;
+        
+    var tempValue = data["list"][0]["main"]["temp"];
     temp.innerHTML = "Temp: "+ tempValue + " °F";
     var windValue = data["list"][0]["wind"]["speed"];
     wind.innerHTML = "Wind: " + windValue + " MPH";
     var humidValue = data["list"][0]["main"]["humidity"];
     humid.innerHTML = "Humidity: " + humidValue +"%";
-    var latValue = data["city"]["coord"]["lat"];
-    var lonValue = data["city"]["coord"]["lat"];
-
+    
     //current date display using moment
     var currentDay = moment().format("L").toString();
-    document.getElementById("currentDay").innerHTML = currentDay;
+    date.innerHTML = currentDay;
 
+    return data;
 
-    // //use a separate API for UV since it was not part of the 5day/3hr interval api, only displayed in current forecast on top
-    // fetch ('https://api.openweathermap.org/data/2.5/onecall?lat='+latValue+'&lon='+lonValue+'&exclude=hourly&appid=dd4aacd5d922b0989a6c7cc5ba3268d7')
-    // .then(response => response.json())
-    // .then(data=> {
-    //     var uvValue = data["current"]["uvi"];
-    //     uv.innerHTML = "UV Index: "+ uvValue;
-    // })
-})     
- .catch(err=> alert("error format!"))
+})
+ //use a separate API for UV since it was not part of the 5day/3hr interval api, only displayed in current forecast on top
+.then((data) => {
+    var latValue = data["city"]["coord"]["lat"];
+    var lonValue = data["city"]["coord"]["lon"];
+    fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+latValue+'&lon='+lonValue+'&exclude=hourly&appid=dd4aacd5d922b0989a6c7cc5ba3268d7')
+    .then(response => response.json())
+    .then(finalData => {
+        var uvValue = finalData["current"]["uvi"];
+        uv.innerHTML = "UV Index: "+ uvValue;
+    })
+})
+
+    .catch(err=> alert("error format!"))
+    
  });
 
